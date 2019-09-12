25ef27152bf3de1aa74548d965474999f64d5803
Martien van den Akker <martien.van.den.akker@darwin-it.nl>
2019-09-12 09:39:20 -0400

    Add changes before reset VM

---
+++ setup/create_kubeaccount.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+SCRIPTPATH=$(dirname $0)
+#
+. $SCRIPTPATH/oek_env.sh
+echo Create helm-user-cluster-admin-role
+cat << EOF | kubectl apply -f -
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: helm-user-cluster-admin-role
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+- kind: ServiceAccount
+  name: default
+  namespace: kube-system
+EOF
+echo Create namespace $K8S_NS
+kubectl create namespace $K8S_NS
+echo kubectl create serviceaccount -n $K8S_NS $K8S_SA
+kubectl create serviceaccount -n $K8S_NS $K8S_SA
