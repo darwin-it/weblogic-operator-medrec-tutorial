98e52b5e5e3aaa6e10738b0da31ec3f604baf11e
oracle <oracle@localhost.localdomain>
2019-10-02 14:18:27 -0400

    make executable

--- setup/oke_env.sh
+++ setup/oke_env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo Set OEK Environment
+echo Set OKE Environment
 export OCID_USER="ocid1.user.oc1..aaaaaaaaskfiokyfunhrkse652q4hfqymhea5xr5xvmhjrsrjgqkgtseyima" 
 export OCID_TENANCY="ocid1.tenancy.oc1..aaaaaaaa6jjldfvjyf7sjmmnq6gltqetamqeizj5fkuxazlmaxh7mbmgrkpa"
 export REGION="eu-frankfurt-1"
